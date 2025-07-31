72a65a793e1d8d9873944a599bed8cd8d1c545a0
artemanistratov-yolo <artem.anistratov@yolo.com>
2025-07-31 07:42:50 -0400

    Practice task

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -42,5 +42,21 @@ switch (userType) {
 let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
+//Dietary task
 
+let user_Role = "Subscriber";
+let access_Level;
 
+if (user_Role === "Employee") {
+    access_Level = "Dietary Services";
+} else if (user_Role === "Enrolled Member") {
+    access_Level = "Dietary Services plus Dietician";
+} else if (user_Role === "Subscriber"){
+    access_Level = "Dietary Services";
+}
+else if (user_Role === "Non-Subscriber"){
+    access_Level = "Return to page with Subscription";
+}
+else {
+    access_Level = "No access granted";
+}
